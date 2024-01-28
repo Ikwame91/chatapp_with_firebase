df0262ff6015abe569645befda4a4f7301e8c515
Kwame Agyapong <Kwameagyapong91@gmail.com>
2024-01-28 22:42:34 +0000

    'de'

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:chat_app_firebase_tutorial/authentication/login_or_register.dart';
+import 'package:chat_app_firebase_tutorial/firebase_options.dart';
 import 'package:chat_app_firebase_tutorial/themes/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
